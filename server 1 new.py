a256ba8fd24e9abf86f49f72b984a4bb2d77aa80
jerisnow <133016493+jerisnow@users.noreply.github.com>
2023-11-30 10:42:21 +0700

    Update server 1 new.py

--- server 1 new.py
+++ server 1 new.py
@@ -4,12 +4,14 @@ import pandas as pd
 import os
 
 class BankServer:
-    def __init__(self, host, port, filename='new-bank.csv'):
+    def __init__(self, host, port):
         self.host = host
         self.port = port
-        self.filename = filename
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen(5)
+        self.filename = 'new-bank.csv'
         self.create_csv_file()
-        self.data = self.import_data_from_csv()
 
     def create_csv_file(self):
         if not os.path.exists(self.filename):
@@ -22,46 +24,71 @@ class BankServer:
             return df
         return pd.DataFrame()
 
-    def start_server(self):
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.bind((self.host, self.port))
-        server.listen()
-
-        print(f"Server listening on {self.host}:{self.port}")
-
-        while True:
-            client, address = server.accept()
-            print(f"Connection from {address} established.")
-            threading.Thread(target=self.handle_client, args=(client,)).start()
+    def export_data_to_csv(self, data):
+        data.to_csv(self.filename, index=False)
 
     def handle_client(self, client_socket):
         while True:
-            request = client_socket.recv(1024).decode()
+            data = client_socket.recv(1024).decode()
 
-            if not request:
-                print("Connection closed.")
+            if not data:
                 break
 
-            if request == "EXPORT_DATA":
-                self.export_data(client_socket)
-            elif request == "IMPORT_DATA":
-                self.import_data(client_socket)
+            if data == "display_data":
+                self.send_data_to_client(client_socket)
+            elif data == "add_data":
+                self.receive_data_and_update("add_data", client_socket)
+            elif data == "update_data":
+                self.receive_data_and_update("update_data", client_socket)
+            elif data == "delete_data":
+                self.receive_data_and_update("delete_data", client_socket)
+            elif data.startswith("search_data"):
+                search_term = data.split(":")[1]
+                self.send_search_results_to_client(client_socket, search_term)
+            elif data == "exit_app":
+                break
 
-    def export_data(self, client_socket):
-        data_str = self.data.to_csv(index=False)
-        client_socket.send(data_str.encode())
+        client_socket.close()
 
-    def import_data(self, client_socket):
-        received_data = client_socket.recv(1024).decode()
-        df = pd.read_csv(pd.compat.StringIO(received_data))
-        self.data = pd.concat([self.data, df], ignore_index=True)
-        self.data.to_csv(self.filename, index=False)
+    def send_data_to_client(self, client_socket):
+        data = self.import_data_from_csv()
+        serialized_data = data.to_csv(index=False).encode()
+        client_socket.send(serialized_data)
 
-        client_socket.send("Data imported successfully.".encode())
+    def receive_data_and_update(self, operation, client_socket):
+        # Receive data from the client
+        data = client_socket.recv(1024).decode()
+        received_data = pd.read_csv(io.StringIO(data))
+
+        # Perform the requested operation
+        if operation == "add_data":
+            current_data = self.import_data_from_csv()
+            updated_data = pd.concat([current_data, received_data], ignore_index=True)
+            self.export_data_to_csv(updated_data)
+
+    def send_search_results_to_client(self, client_socket, search_term):
+        data = self.import_data_from_csv()
+
+        if search_term == "all":
+            search_results = data
+        else:
+            search_results = data[data['Nama'].str.contains(search_term, case=False)]
+
+        serialized_results = search_results.to_csv(index=False).encode()
+        client_socket.send(serialized_results)
+
+    def start(self):
+        print(f"Server listening on {self.host}:{self.port}")
+
+        while True:
+            client_socket, client_address = self.server_socket.accept()
+            print(f"Accepted connection from {client_address}")
+
+            client_handler = threading.Thread(target=self.handle_client, args=(client_socket,))
+            client_handler.start()
 
 if __name__ == "__main__":
-    host = "your_server_ip"  # Replace with your actual server IP
-    port = 5000  # Choose a port
-
+    host = "10.200.18.137"
+    port = 5005
     server = BankServer(host, port)
-    server.start_server()
+    server.start()
